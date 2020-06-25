b8162de6e46accfb3355654ca90220e7e4ce813e
Edward A Maxedon, Sr <edadma@vinctus.com>
2020-06-25 11:19:13 -0400

    cont coding insertions

--- src/main/scala/com/vinctus/oql/Resource.scala
+++ src/main/scala/com/vinctus/oql/Resource.scala
@@ -1,5 +1,15 @@
 package com.vinctus.oql
 
+import scala.collection.immutable.ListMap
+import scala.concurrent.Future
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSExport
+
 class Resource private[oql] (oql: OQL, name: String, entity: Entity) {
-  def getMany() = {}
+  private val builder = oql.queryBuilder.project(name)
+
+  @JSExport
+  def getMany(): Future[List[ListMap[String, Any]]] = builder.getMany
+
+  def insert(obj: js.Any): js.Any = {}
 }
